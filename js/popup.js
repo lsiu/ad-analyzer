3dd382fa4b08f8a124729d1773396bc108312ec0
Leonard Siu <831085+lsiu@users.noreply.github.com>
2025-09-28 17:16:03 +0800

    clean up popup code that was not required after we move to react / webpack modularize solution

--- js/popup.js
+++
@@ -1,7 +0,0 @@
-// js/popup.js - Popup script entry point
-
-import { refreshAds } from './modules/popupHandler.js';
-
-document.addEventListener('DOMContentLoaded', function() {
-  refreshAds();
-});
