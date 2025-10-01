3ee3059270ba2bf08e1c552cbd2f0131ae38068d
Leonard Siu <831085+lsiu@users.noreply.github.com>
2025-10-01 09:39:51 +0800

    remove dead and debugger

--- src/openrtb_panel.jsx
+++ src/openrtb_panel.jsx
@@ -5,5 +5,4 @@ import OpenRTBPanel from './OpenRTBPanel';
 const container = document.getElementById('root');
 const root = createRoot(container);
 console.log('Rendering OpenRTBPanel');
-debugger;
 root.render(<OpenRTBPanel/>);
