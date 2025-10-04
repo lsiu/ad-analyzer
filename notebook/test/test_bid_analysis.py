bd73caeb840066e1d4c898776ad674cb8bfd90f1
Leonard Siu <831085+lsiu@users.noreply.github.com>
2025-10-04 18:15:58 +0800

    fix sometime the impid in request is str but the resposne is int type mismatch problem.

--- notebook/test/test_bid_analysis.py
+++ notebook/test/test_bid_analysis.py
@@ -278,5 +278,74 @@ class TestBidAnalysisFunctions(unittest.TestCase):
         self.assertEqual(placement_data, {})
 
 
+    def test_extract_request_and_response_placement_id_integration_with_int_str_type_difference_in_impid(self):
+        """Integration test for extracting placement IDs and matching with bids, handling int/str type differences"""
+        requests = [
+            {
+                'requestId': 'req_001',
+                'body': json.dumps({
+                    'imp': [
+                        {
+                            'id': '1',  # impid as string but string in response
+                            'ext': {
+                                'gpid': 'placement_1'
+                            }
+                        },
+                        {
+                            'id': 2,  # impid as integer but integer in response
+                            'tagid': 'placement_2'
+                        }
+                    ]
+                })
+            }
+        ]
+        
+        responses = [
+            {
+                'requestId': 'req_001',
+                'statusCode': 200,
+                'body': json.dumps({
+                    'seatbid': [
+                        {
+                            'bid': [
+                                {
+                                    'impid': 1,  # intger impid but string in request
+                                    'price': 2.00,
+                                    'adomain': ['adv1.com'],
+                                    'crid': 'creative_1'
+                                },
+                                {
+                                    'impid': '2',  # string impid but integer in request
+                                    'price': 3.00,
+                                    'adomain': ['adv2.com'],
+                                    'crid': 'creative_2'
+                                }
+                            ]
+                        }
+                    ],
+                    'cur': 'USD'
+                })
+            }
+        ]
+        
+        placement_map = extract_placements_from_requests(requests)
+        bids = extract_bids_from_responses(responses)
+        
+        # Add placement IDs to bid data by matching request and impression IDs
+        for bid in bids:
+            req_id = bid['request_id']
+            imp_id = bid['impid']  # This is actually the impid from the bid
+            
+            # Look up the actual placement ID from our mapping
+            if req_id in placement_map and str(imp_id) in placement_map[req_id]:
+                bid['placement_id'] = placement_map[req_id][str(imp_id)]
+            else:
+                bid['placement_id'] = 'unknown_placement_id'
+        
+        self.assertEqual(len(bids), 2)
+        self.assertEqual(bids[0]['placement_id'], 'placement_1')
+        self.assertEqual(bids[1]['placement_id'], 'placement_2')
+
+
 if __name__ == '__main__':
     unittest.main()
